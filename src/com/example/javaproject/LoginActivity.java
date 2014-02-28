a7b1b1d006b00527d7e46b958299186f694b12e7
benjaminsenechal <benjamin.senechal1@epsi.fr>
2014-02-28 09:11:00 +0100

    first commit

---
+++ src/com/example/javaproject/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.example.javaproject;
+
+import com.epsi.eiwd306.Provider;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		Provider.getAllPeople();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+
+}
