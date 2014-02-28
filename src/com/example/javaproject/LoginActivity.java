763a79f91a9adfcde19b1a1f24cdbf340abb8898
benjaminsenechal <benjamin.senechal1@epsi.fr>
2014-02-28 10:12:32 +0100

    Implements login activity

--- src/com/example/javaproject/LoginActivity.java
+++ src/com/example/javaproject/LoginActivity.java
@@ -1,25 +1,59 @@
 package com.example.javaproject;
 
 import com.epsi.eiwd306.Provider;
+import com.epsi.eiwd306.beans.User;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.app.Activity;
-import android.view.Menu;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends Activity implements View.OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
-		Provider.getAllPeople();
-	}
+		
+		Button button = (Button)findViewById(R.id.buttonConnexion);
+		button.setOnClickListener(this);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.login, menu);
-		return true;
 	}
+	
+	public void onClick(View v){
+		Button caller = (Button)v;
+		EditText login = (EditText)findViewById(R.id.loginText);
+		EditText password = (EditText)findViewById(R.id.passwordText);
+		User u;
+		
+		if(caller.getId() == R.id.buttonConnexion){	
+			u = Provider.login(login.getText().toString(), password.getText().toString());
+			if (u == null){
+				System.out.println("Fail");
+				AlertDialog.Builder builder = new AlertDialog.Builder(this);
+				builder
+				.setTitle("Erreur")
+				.setMessage("Mauvais login ou mot de passe")
+				.setPositiveButton(
+						"OK", 
+						new DialogInterface.OnClickListener(){
+							@Override
+							public void onClick(DialogInterface dialog, int which) {
+								dialog.cancel();
+							}
+				 })
+				.show();
+			}else{
+				Intent intent = new Intent(this, HomeActivity.class);
+			    startActivity(intent);
+			    finish();
+			}
+		}
 
+	}
 }
