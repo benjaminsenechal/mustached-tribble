53f412c5214d896fe381e1cd11c9fd4d1c4ffda2
benjaminsenechal <benjamin.senechal1@epsi.fr>
2014-03-15 11:48:22 +0100

    Add landscape view for details view and Splashscreen

--- gen/com/example/javaproject/R.java
+++ gen/com/example/javaproject/R.java
@@ -21,12 +21,14 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_item=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080016;
+        public static final int action_settings=0x7f080017;
         public static final int buttonConnexion=0x7f080010;
         public static final int imageView1=0x7f080013;
+        public static final int imageView2=0x7f080016;
         public static final int imageViewAvatar=0x7f080000;
         public static final int listView=0x7f080011;
         public static final int listViewAbilities=0x7f080008;
@@ -53,13 +55,15 @@ public final class R {
         public static final int activity_home=0x7f030001;
         public static final int activity_login=0x7f030002;
         public static final int activity_results=0x7f030003;
-        public static final int template_abilities=0x7f030004;
-        public static final int template_persons=0x7f030005;
+        public static final int activity_splashscreen=0x7f030004;
+        public static final int template_abilities=0x7f030005;
+        public static final int template_persons=0x7f030006;
     }
     public static final class menu {
         public static final int home=0x7f070000;
         public static final int login=0x7f070001;
         public static final int results=0x7f070002;
+        public static final int splashscreen=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -68,6 +72,7 @@ public final class R {
         public static final int title_activity_details=0x7f050005;
         public static final int title_activity_home=0x7f050003;
         public static final int title_activity_results=0x7f050004;
+        public static final int title_activity_splashscreen=0x7f050006;
     }
     public static final class style {
         /** 
