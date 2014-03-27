204d1d72bf6581f70d29d2cd58fb79d2301d39a2
benjaminsenechal <benjamin.senechal1@epsi.fr>
2014-03-27 12:18:59 +0100

    Add custom buttons

--- gen/com/example/javaproject/R.java
+++ gen/com/example/javaproject/R.java
@@ -10,15 +10,20 @@ package com.example.javaproject;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int blue=0x7f040000;
+        public static final int white=0x7f040001;
+    }
     public static final class dimen {
-        /**  Default screen margins, per the Android Design guidelines. 
-
+        /** 
          Customize dimensions originally defined in res/values/dimens.xml (such as
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int layoutPaddingHorizontal=0x7f050003;
+        public static final int layoutPaddingVertical=0x7f050002;
     }
     public static final class drawable {
         public static final int ic_item=0x7f020000;
@@ -26,30 +31,28 @@ public final class R {
         public static final int splashscreen=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080017;
-        public static final int buttonConnexion=0x7f080011;
-        public static final int imageView1=0x7f080015;
-        public static final int imageView2=0x7f080016;
-        public static final int imageViewAvatar=0x7f080000;
-        public static final int linearLayout1=0x7f080014;
-        public static final int listView=0x7f080012;
-        public static final int listViewAbilities=0x7f080009;
-        public static final int loginText=0x7f08000f;
-        public static final int nomEditText=0x7f08000a;
-        public static final int nomTextView=0x7f08000d;
-        public static final int passwordText=0x7f080010;
-        public static final int prenomEditText=0x7f08000b;
-        public static final int prenomTextView=0x7f08000c;
-        public static final int rechercheButton=0x7f08000e;
-        public static final int textView1=0x7f080005;
-        public static final int textViewAbility=0x7f080013;
-        public static final int textViewDescription=0x7f080007;
-        public static final int textViewEmail=0x7f080003;
-        public static final int textViewFirstname=0x7f080001;
-        public static final int textViewLabelDesc=0x7f080004;
-        public static final int textViewLabelTel=0x7f080006;
-        public static final int textViewLastname=0x7f080002;
-        public static final int textViewPhoneNumber=0x7f080008;
+        public static final int action_settings=0x7f090015;
+        public static final int buttonConnexion=0x7f09000f;
+        public static final int imageView1=0x7f090013;
+        public static final int imageView2=0x7f090014;
+        public static final int imageViewAvatar=0x7f090000;
+        public static final int linearLayout1=0x7f090012;
+        public static final int listView=0x7f090010;
+        public static final int listViewAbilities=0x7f090009;
+        public static final int loginText=0x7f09000d;
+        public static final int nomEditText=0x7f09000b;
+        public static final int passwordText=0x7f09000e;
+        public static final int prenomEditText=0x7f09000a;
+        public static final int rechercheButton=0x7f09000c;
+        public static final int textView1=0x7f090005;
+        public static final int textViewAbility=0x7f090011;
+        public static final int textViewDescription=0x7f090007;
+        public static final int textViewEmail=0x7f090003;
+        public static final int textViewFirstname=0x7f090001;
+        public static final int textViewLabelDesc=0x7f090004;
+        public static final int textViewLabelTel=0x7f090006;
+        public static final int textViewLastname=0x7f090002;
+        public static final int textViewPhoneNumber=0x7f090008;
     }
     public static final class layout {
         public static final int activity_details=0x7f030000;
@@ -61,19 +64,19 @@ public final class R {
         public static final int template_persons=0x7f030006;
     }
     public static final class menu {
-        public static final int home=0x7f070000;
-        public static final int login=0x7f070001;
-        public static final int results=0x7f070002;
-        public static final int splashscreen=0x7f070003;
+        public static final int home=0x7f080000;
+        public static final int login=0x7f080001;
+        public static final int results=0x7f080002;
+        public static final int splashscreen=0x7f080003;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int title_activity_details=0x7f050005;
-        public static final int title_activity_home=0x7f050003;
-        public static final int title_activity_results=0x7f050004;
-        public static final int title_activity_splashscreen=0x7f050006;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060002;
+        public static final int title_activity_details=0x7f060005;
+        public static final int title_activity_home=0x7f060003;
+        public static final int title_activity_results=0x7f060004;
+        public static final int title_activity_splashscreen=0x7f060006;
     }
     public static final class style {
         /** 
@@ -97,10 +100,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+        public static final int CustomButton=0x7f070002;
     }
 }
